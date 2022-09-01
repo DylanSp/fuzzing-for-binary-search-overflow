0a795ee419219d37fb748ae6ca7d76f276fbe52e
Dylan Sprague <ithilanor@gmail.com>
2022-09-01 02:44:47 +0000

    Add actual assertions to fuzz testing

--- search/binary_search_test.go
+++ search/binary_search_test.go
@@ -2,6 +2,8 @@ package search
 
 import (
 	"testing"
+
+	"golang.org/x/exp/slices"
 )
 
 func TestBinarySearchFindsExistingElement(t *testing.T) {
@@ -32,8 +34,18 @@ func FuzzBinarySearch(f *testing.F) {
 	f.Fuzz(func(t *testing.T, sliceLength uint8, target uint8) {
 		ints := createSortedFilledSlice(sliceLength)
 
-		// findResult := BinarySearch(ints, target)
-		BinarySearch(ints, target)
+		findResult := BinarySearch(ints, target)
+
+		sliceActuallyContainsTarget := slices.Contains(ints, target)
+		if sliceActuallyContainsTarget && findResult == nil {
+			t.Error("Binary search didn't find existing element")
+		} else if !sliceActuallyContainsTarget && findResult != nil {
+			t.Error("Binary search erroneously found nonexisting element")
+		}
+
+		if findResult != nil && (ints[*findResult] != target) {
+			t.Error("Binary search found incorrect element")
+		}
 	})
 }
 
